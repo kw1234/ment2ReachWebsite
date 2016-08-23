c7a3ffbe6606d8cc1cff0decb5834e5e053ce4b6
Sajeena Warrier <kawarrier@yahoo.com>
2016-08-22 23:08:34 -0700

    lala

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
+//for form post handling
+var bodyParser = require('body-parser');
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+app.use(bodyParser.json());
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
+
 
 console.log("in index.js");
 
@@ -29,6 +38,20 @@ app.get('/dashboard', function(request, response) {
 app.get('/cool', function(request, response) {
     response.send(cool());
 });
+
+app.post('/dashboard', function (req, res) {
+	var email = req.body.email;
+	var password = req.body.psw;
+	console.log("dashboard post email");
+	console.log(email);
+	console.log("dashboard post password");
+	console.log(password);
+	app.get('/loginpage', function(req, res, next) {
+  		res.json({ message: 'Hello World' });
+	});
+});
+
+
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
